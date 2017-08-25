c90c62630e6e663959354b2599581cb7b5d8c5c2
U52444 <U52444@A159LBJX.ustr.com>
2017-08-25 17:52:25 -0600

    Proyecto completo hasta hoy

---
+++ DB/promociones.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 25-08-2017 a las 23:51:11
+-- Versión del servidor: 5.6.12-log
+-- Versión de PHP: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `foodkart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `promociones`
+--
+
+CREATE TABLE IF NOT EXISTS `promociones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `productosids` text NOT NULL,
+  `precio` int(11) NOT NULL,
+  `fechainicio` date NOT NULL,
+  `fechafinal` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
